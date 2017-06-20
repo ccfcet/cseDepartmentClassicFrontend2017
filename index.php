474e9b9e0bda86588843d2d231cded6dbe80d671
Jilvin <jilvinjacob@gmail.com>
2017-06-20 15:59:27 +0530

    mainMenu design complete

--- index.php
+++ index.php
@@ -71,19 +71,22 @@
       <div id="mainMenuSub">
         <span id="mainMenuSubTitle">Information for:</span>
         <ul>
-          <li id="mainMenuSubFirstItem" onclick="location.href='#';"><a href="#">Students</a></li>
-          <ul id="AcademicsNestedList" class="mainMenuNested">
-            <li onclick="location.href='#';"><a href="#">Academic Programs</a></li>
-            <li onclick="location.href='#';"><a href="#">Faculties</a></li>
-            <li onclick="location.href='#';"><a href="#">Awards and Highlights</a></li>
-            <li onclick="location.href='#';"><a href="#">Facilities</a></li>
-            <li onclick="location.href='#';"><a href="#">Library</a></li>
-            <li onclick="location.href='#';"><a href="#">Academic Resources</a></li>
+          <li id="StudentsNestedListButton" class="mainMenuSubFirstItem" onclick="toggleMenuNestedList(this)"><a href="#">Students</a><span id="glyphiconStudents" class="glyphicon glyphicon-chevron-down"></span></li>
+          <ul id="StudentsNestedList" class="mainMenuSubNested">
+            <li onclick="location.href='#';"><a href="#">Communities and Groups</a></li>
           </ul>
           <li onclick="location.href='#';"><a href="#">Faculty</a></li>
           <li onclick="location.href='#';"><a href="#">Alumni</a></li>
           <li onclick="location.href='#';"><a href="#">Employees</a></li>
-          <li onclick="location.href='#';"><a href="#">Community</a></li>
+          <li id="CommunityNestedListButton" onclick="toggleMenuNestedList(this)"><a href="#">Community</a><span id="glyphiconCommunity" class="glyphicon glyphicon-chevron-down"></span></li>
+          <ul id="CommunityNestedList" class="mainMenuSubNested">
+            <li onclick="location.href='#';"><a href="#">Neighbours and Nonprofits</a></li>
+            <li onclick="location.href='#';"><a href="#">Parents</a></li>
+            <li onclick="location.href='#';"><a href="#">Conference and Event Services</a></li>
+            <!-- <li onclick="location.href='#';"><a href="#">Facilities</a></li> -->
+            <!-- <li onclick="location.href='#';"><a href="#">Library</a></li> -->
+            <!-- <li onclick="location.href='#';"><a href="#">Academic Resources</a></li> -->
+          </ul>
         </ul>
       </div>
     </div>
