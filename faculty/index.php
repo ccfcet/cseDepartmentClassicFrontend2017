7906fbf4503c8424ff557e39f18ec4604cf6f434
NanduKMG <nandukrishnan55555@gmail.com>
2017-06-23 17:31:54 +0530

    converting to PHP

--- faculty/index.php
+++ faculty/index.php
@@ -1,71 +1,24 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-
-  <title>Department of Computer Science & Engineering, CET</title>
-  <meta name="description" content="Official website of Department of Computer Science & Engineering">
-  <meta name="author" content="Web Team, CET">
-
-  <!--[if lt IE 9]>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-  <![endif]-->
 
 
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Google icons-->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<!-- Latest compiled and minified CSS - Bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-  <!-- Google icons-->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="<?php echo SITE_SERVER_BASE ?>faculty/css/style.php">
 
-  <!-- Latest compiled and minified CSS - Bootstrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-  <link rel="stylesheet" type="text/css" href="css/style.css">
 
-</head>
-
-<body>
-
-  <div class="menuButtonContainer" id="mainMenuButton" onclick="menuButtonToggle(this); actionNav(this);">
-    <div class="bar1"></div>
-    <div class="bar2"></div>
-    <div class="bar3"></div>
-  </div>
-
-  <div id="mainMenu">
-      <h4 id="menuTitle">Menu</h4>
-      <a href="#" class="active">Home</a>
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-      <a href="#">Link 4</a>
-    </ul>
-    <!-- <div id="mainMenuSub">
-      Information for:
-    </div> -->
-  </div>
-
-  <div class="container-fluid" id="mainContent">
-    <div id="mainContentOverlay">
-    </div>
-    <div class="row" id="headerRow">
-      <h4 id="headingDepartmentText">Department of Computer Science & Engineering</h4>
-      <h6 id="headingCollegeText">College of Engineering Trivandrum</h6>
-    </div>
-
-    <div id="profDP">
+<div id="profDP">
     <img id="profIMG" class="img-circle" src="nizar.jpg" alt="Dr.Abdul Nizar"  />
-    </div>
+</div>
 
-    <div id="profName">
+<div id="profName">
     <h2 class="text-center">Dr Abdul Nizar</h2>
-    </div>
+</div>
 
-    <div class="row">
+<div class="row">
 
       <div class="col-md-3 card" style="margin-left:5%; ">
          <div class="cardHeader">
@@ -166,9 +119,10 @@
         </div>
       </div>
 
-    </div>
+</div>
 
-    <div class="row">
+
+<div class="row">
       
 
       <div class="col-md-4 card">
@@ -180,16 +134,11 @@
     </div>
 
 
-  </div>
+</div>
 
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
   <!-- Latest compiled and minified JavaScript - Bootstrap -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-  <script src="js/common.js"></script>
-
-</body>
-
-</html>
+  
