84afb68306caf1008ea785f42f8fa948d2bf1796
NanduKMG <nandukrishnan55555@gmail.com>
2017-06-24 23:31:48 +0530

    added more data

--- faculty/abdulnizarData.php
+++ faculty/abdulnizarData.php
@@ -8,12 +8,15 @@
 
 
 <div id="profDP">
+
   <div id="profIMG" class="img-circle" ></div>
 
 </div>
 
 <div id="profName">
+
   <h2 class="text-center">Dr Abdul Nizar</h2>
+
 </div>
 
 <div class="row">
@@ -120,13 +123,157 @@
 </div>
 
 
-<div class="row">
+ <div class="row">
+      <!-- ADDED on june 24 NOT UPDATED WITH PHPFIED VERSION -->
 
+      <div class="col-md-4 card">
+        <div class="cardHeader">
+           <p><i class="material-icons" style="font-size:20px;color:#0077FF;padding-left: 5%;" >grade</i>Professional Experience</p>  
+        </div>
 
-  <div class="col-md-4 card">
-    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+              <p> 1990 - 1992</p>
+            </div>
+
+            <div class="col-md-10">
+              <p> Worked as Engineer, R & D in Transmatic systems Trivandrum</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-2">
+              <p> </p>
+            </div>
+
+            <div class="col-md-10">
+              <p>  Working as a faculty member in various engineering colleges under the Directorate of Technical Education Government of Kerala for 21 years</p>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-md-2">
+              <p> </p>
+            </div>
+
+            <div class="col-md-10" >
+              <p>  working  as Head of the Department in the Department of Computer Science and Engineering, College of Engineering Trivandrum for the past two years until June 2016</p>
+            </div>
+            
+        </div>
+         
+      </div>
+
+      <div class="col-md-7 card">
+        <div class="cardHeader">
+           <p><i class="material-icons" style="font-size:20px;color:#0077FF;padding-left: 5%;" >grade</i>Contributions in professional front</p>  
+        </div>
+
+        <div class="row">
+            <div class="col-md-2">
+              <p>  1996 – 1997</p>
+            </div>
+
+            <div class="col-md-10">
+              <p> Co-coordinator of the IMPACT Project (Ministry Human Resources, Government of India) in the Department of Computer Science & Engineering, College of Engineering, Trivandrum</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-2">
+              <p> 2003-2005</p>
+            </div>
+
+            <div class="col-md-10">
+              <p>  Co-coordinator of the TEQIP Project ((Ministry Human Resources, Government of India and World Bank in the Department of Computer Science & Engineering, College of Engineering, Trivandrum</p>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-md-2">
+              <p>2003-2005</p>
+            </div>
+
+            <div class="col-md-10" >
+              <p> Chairman of the Course Material Development Committee for Database Management Systems under the EDUGRID Project of Indian Institute of Information Technology and Management, Kerala (IIITMK)</p>
+            </div>
+            
+        </div>
+
+        <div class="row">
+            <div class="col-md-2">
+              <p> November-December 2004</p>
+            </div>
+
+            <div class="col-md-10" >
+              <p>  Chairman of B-Tech Degree Examinations of University of Kerala</p>
+            </div>
+            
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+              <p> 2000 - 2001</p>
+            </div>
+
+            <div class="col-md-10" >
+              <p>  Member of the syllabus committee for B-Tech degree courses of Kerala University</p>
+            </div>
+            
+        </div>
+         
+      </div>
 
     </div>
-  </div>
 
-</div>
+
+
+
+    <div class="row">
+      <!-- to be ADDED on june 24 NOT UPDATED WITH PHPFIED VERSION -->
+
+      <div class="col-md-11 card" >
+        <div class="cardHeader">
+           <p><i class="material-icons" style="font-size:20px;color:#0077FF;padding-left: 5%;" >grade</i>Publications</p>  
+        </div>
+         
+        <!--IMPLEMENT THE BELOW THING WITH LISTS!!!!-->
+        <div class="row">
+        <!--<div class="col-md-1">
+        </div>-->
+        <div class="col-md-12" style="padding-left: 3%">    
+          <p><i class="material-icons" style="font-size:10px;color:#0077FF;" >grade</i> Abdul Nizar M and P. Sreenivasa Kumar, Ordered Twig Query processing Over XML Streams, International Symposium on Data, Information and Knowledge Spectrum (ISDIKS 2007), Cochin, India.</p>
+
+ 
+
+          <p>  <i class="material-icons" style="font-size:10px;color:#0077FF;" >grade</i>Abdul Nizar M., P. Sreenivasa Kumar, Efficient Evaluation of Forward XPath Axes over XML Streams, 14th International Conference on Management of Data (COMAD 2008), Mumbai, India.</p>
+
+ 
+
+          <p> <i class="material-icons" style="font-size:10px;color:#0077FF;" >grade</i>Abdul Nizar M., P. Sreenivasa Kumar, Ordered Backward XPath Axes Processing against XML Streams, Sixth International VLDB XML Database Symposium (Xsym 2009), pages 1-16, August 2009, Springer LNCS, Vol. 5679.</p>
+
+ 
+
+          <p><i class="material-icons" style="font-size:10px;color:#0077FF;" >grade</i> Abdul Nizar M., G. Suresh Babu, P. Sreenivasa Kumar, “SFilter: A Simple, Scalable Filter for XML Streams'', 15th International Conference on Management of Data (COMAD 2009), pages 136-141, December 2009.</p>
+
+ 
+
+ 
+
+          <p> <i class="material-icons" style="font-size:10px;color:#0077FF;" >grade</i>Abdul Nizar M., P. Sreenivasa Kumar, “Order-Aware Twigs: Adding Order Semantics to Twigs”, Journal of Information and Data Management, 2011.</p>
+
+ 
+
+          <p><i class="material-icons" style="font-size:10px;color:#0077FF;" >grade</i> Order-aware Graphs – A Representation of XPath Expressions with Ordered Axes, Technical Report, Department of Computer Science and Engineering, IIT Madras</p>
+
+
+
+        </div>
+      </div>
+
+      
+    </div>
+
+
+  </div>
